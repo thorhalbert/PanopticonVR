28fba0b8b636dfb57320e3e75d4660cbc93ce285
Thor Halbert <thor@coolearth.com>
2022-09-02 18:08:45 -0700

    Start working on next steps, cleanup, reorganization

---
+++ TestSSH/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Renci.SshNet;
+
+Console.WriteLine("Hello, World!");
+
+
+var ci = new ConnectionInfo("house.thorsbrain.com", "neoworlds",
+    new PrivateKeyAuthenticationMethod("neoworlds", new PrivateKeyFile(@"C:\cygwin64\home\thor\.creds\neoworlds.pri")));
+
+
+using (var client = new SshClient(ci))
+{
+    client.HostKeyReceived += (sender, e) =>
+    {
+        Console.WriteLine(Convert.ToHexString(e.FingerPrint));
+    
+    };
+    client.Connect();
+}
