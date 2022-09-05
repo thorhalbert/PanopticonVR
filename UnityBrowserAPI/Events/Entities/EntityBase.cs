76ca916c616676dc20cf200fd8af479e220f0b3e
Thor Halbert <thor@coolearth.com>
2022-09-05 10:42:32 -0700

    More cleanup.   Start the 'worlds' cli for provisioning/maintenance (easier then blazor maint, which we'll do someday and hopefully have common libraries)

--- UnityBrowserAPI/Events/Entities/EntityBase.cs
+++ UnityBrowserAPI/Events/Entities/EntityBase.cs
@@ -3,6 +3,20 @@ using System.Numerics;
 
 namespace UnityBrowserAPI.Events.Entities
 {
+    /// <summary>
+    /// Ultimately the idea here is to look something like a video compression codec
+    /// where we compute a vector (possibly affected by gravity physics) where the receivers
+    /// can compute the current position, then publish an absolute position/fix occasionally if the
+    /// estimated value exceeds an error tolerance, like a keyframe.   Initially we will spray all of the
+    /// movement data uncompressed, so we can get a feel for it to come up with a wait to fair a curve
+    /// to estimate the vector.   We do want this data to have as little lag as possible.
+    /// 
+    /// We may also have problems with competing physics engines, or possibily bad actors operating
+    /// as the browser.   So, imagine 10,000 browsers watching a baseball game or a billiards tournament.
+    /// Everyone must agree on where everything is.
+    /// 
+    /// If a tree falls in a virtual forest and nobody is viewing the scene, then did it fall?   Answer is no.
+    /// </summary>
     public abstract class EntityBase : ReporterBase
     {
         public Guid ManufacturerId { get; set; }
---
+++ worlds/Program.cs
@@ -0,0 +1,4 @@
+﻿// Worlds CLI (command line)
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
